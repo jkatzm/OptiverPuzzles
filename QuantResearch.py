c50446192a295fef2417ec88254af491fcb8b045
Jordan Katz <jordankatz@Jordans-MBP-2.hsd1.il.comcast.net>
2020-01-23 13:36:54 -0600

    clean

--- QuantResearch.py
+++ QuantResearch.py
@@ -16,23 +16,78 @@ when the game is played among four players?
 """
 
 
-
 from random import choice
 from itertools import permutations
 from fractions import Fraction
 
+###############################################################
+# Utility Functions
+###############################################################
 
 def avg(x):
-	# x: list of floats/ints
 	return round(sum(x)/len(x), 10)
 
+def print_dict(d, level):
+	# d: dict
+	# level: int
+	if level == 0:
+		print("optimal choices:")
+
+	for k,v in d.items():
+		if type(v) == dict:
+			print(k, ":")
+			print_dict(v, level+1)
+		else:
+			space = ""
+			for i in range(level):
+				space += "  "
+			print(space, k, ":")
+			for j in v:
+				print(j)
+	print()
+
+def transform_dict(d):
+	# d: dict
+	new_dict = {}
+
+	for key,vals in d.items():
+		outer_key = key[:-1]
+		inner_key = key[-1]
+
+		if outer_key not in new_dict:
+			new_dict[outer_key] = {}
+
+		new_dict[outer_key][inner_key] = vals
+
+	return new_dict
+
+
+###############################################################
+# Game-specific Functions
+###############################################################
+
+def get_fractions():
+	# returns the discretized state-space we work on:
+	# the list of possible choices for each player
+
+	eps = 1 / 10000000
+	fractions = set([0, 1, eps, 1-eps])
+	# fractions.add(2*eps)
+	# fractions.add(1-2*eps)
+
+	for denom in [2,4,6,8]:
+		for num in range(1, denom):
+			f = num/denom # Fraction(num, denom)
+			fractions.add(f)
+			fractions.add(f + eps)
+			fractions.add(f - eps)
+
+			# fractions.add(f + 2 * eps)
+			# fractions.add(f - 2 * eps)
+	return fractions
 
 def prob_win(x, others):
-	# x: float
-	# others: list of floats
-	#
-	# returns the probability that player "x" wins given what the others play
-	# ASSUMES ALL VALUES DISTINCT
+	# returns the probability that someone playing "x" wins given what the others play
 
 	value = -2
 
@@ -56,42 +111,11 @@ def prob_win(x, others):
 
 	return round(value, 10)
 
-
-def print_dict(d, level):
-	# d: dict
-	# level: int
-	for k,v in d.items():
-		if type(v) == dict:
-			print(k, ":")
-			print_dict(v, level+1)
-		else:
-			space = ""
-			for i in range(level):
-				space += "  "
-			print(space, k, ":")
-			for j in v:
-				print(j)
-	print()
-
-
-def transform_dict(d):
-	# d: dict
-	new_dict = {}
-
-	for key,vals in d.items():
-		outer_key = key[:-1]
-		inner_key = key[-1]
-
-		if outer_key not in new_dict:
-			new_dict[outer_key] = {}
-
-		new_dict[outer_key][inner_key] = vals
-
-	return new_dict
-
-
 def make_next_dict(d):
 	# d: dict
+	# 
+	# memoization, gives optimal choices for each player at different game "levels"
+
 	next_dict = {}
 
 	for prev in d.keys():
@@ -107,7 +131,6 @@ def make_next_dict(d):
 
 			else:
 				p_win = avg([prob_win(choice, prev + post) for post in d[prev][choice]])
-
 
 			if p_win > optimal_P:
 				optimal_P = p_win
@@ -130,60 +153,52 @@ def make_next_dict(d):
 
 	return next_dict
 
-
-
-def get_fractions():
-	# fractions = [i/80 for i in range(0, 80+1)]
-
-	eps = 1 / 10000000
-	fractions = set([0, 1, eps, 1-eps])
-	# fractions.add(2*eps)
-	# fractions.add(1-2*eps)
-
-	for denom in [2,3,4,5,6]:
-		for num in range(1, denom):
-			f = num/denom # Fraction(num, denom)
-			fractions.add(f)
-			fractions.add(f + eps)
-			# fractions.add(f + 2 * eps)
-			fractions.add(f - eps)
-			# fractions.add(f - 2 * eps)
-	return fractions
+###############################################################
+###############################################################
+###############################################################
 
 
 
 
-n_players = int(input("number of players?\n"))
-print("\nsolving the problem for", n_players, "players...")
-assert(n_players > 1)
+n_players = int(input("\nEnter number of players: "))
+assert(1 < n_players < 6)
+
+
+
+print("\nSOLVING...")
+print("using backwards induction on a modified (finite, discrete) state-space")
 
 X = {i : [] for i in permutations(get_fractions(), n_players)}
 
 for i in range(n_players):
 	X = make_next_dict(transform_dict(X))
 
-print("\n...solved.\n")
+print("...SOLVED!\n")
 print_dict(X, 0)
 
+print("strategy profiles for", n_players, "players:")
 
 if n_players == 2:
-	print("strategy profiles:")
 	print("A : 1/2")
 	print("B : random selection from {1/2 + epsilon, 1/2 - epsilon}")
 	pass
 
 elif n_players == 3:
-	print("strategy profiles:")
 	print("A : random selection from {1/4, 3/4}")
 	print("B : {1/4 if A==3/4; 3/4 if A==1/4}")
 	print("C : random selection from (1/4, 3/4)")
 
 elif n_players == 4:
-	print("strategy profiles:")
 	print("A : random selection from {1/6, 5/6}")
 	print("B : {1/6 if A==5/6; 5/6 if A==1/6}")
 	print("C : 1/2")
 	print("D : random selection from (1/6, 1/2) U (1/2, 5/6)")
 
-
+elif n_players == 5:
+	print("(WARNING -- May be incorrect due to finite state-space approximation)")
+	print("A : random selection from {1/8, 7/8}")
+	print("B : random selection from {3/8, 5/8}")
+	print("C : {1/8 if A==7/8; 7/8 if A==1/8}")
+	print("D : {3/8 if B==5/8; 5/8 if B==3/8}")
+	print("E : random selection from (1/8, 3/8) U (3/8, 5/8) U (5/8, 7/8)")
 
